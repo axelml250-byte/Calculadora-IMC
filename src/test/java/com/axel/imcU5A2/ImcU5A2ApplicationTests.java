48c8cda1db36c57b64ffb2d3c1cf43cb200a6d59
Axel07 <axelml250@gmail.com>
2026-05-29 16:33:47 -0600

    Inicio de calculadora: Estructura base

---
+++ src/test/java/com/axel/imcU5A2/ImcU5A2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.imcU5A2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcU5A2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
